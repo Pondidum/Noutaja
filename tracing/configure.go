eb2343e6eeec57ce091ffba23b024d9728f030e6
andy <andy@sammalmaa.fi>
2024-02-18 23:42:23 +0200

    tracing: remove configuration, its all doable with environment variables by default

--- tracing/configure.go
+++ tracing/configure.go
@@ -3,11 +3,8 @@ package tracing
 import (
 	"context"
 	"fmt"
-	"net"
-	"net/url"
 	"os"
 	"os/signal"
-	"regexp"
 	"strings"
 	"syscall"
 
@@ -17,15 +14,13 @@ import (
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
 const OtlpEndpointEnvVar = "OTEL_EXPORTER_OTLP_ENDPOINT"
 const OtlpTracesEndpointEnvVar = "OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"
-const OtlpHeadersEnvVar = "OTEL_EXPORTER_OTLP_HEADERS"
-const OtelTraceExporterEnvVar = "OTEL_TRACE_EXPORTER"
+const OtelTraceExporterEnvVar = "OTEL_TRACES_EXPORTER"
 
 func Configure(ctx context.Context, appName string, version string) (func(ctx context.Context) error, error) {
 	exporter, err := createExporter(ctx)
@@ -63,30 +58,7 @@ func Configure(ctx context.Context, appName string, version string) (func(ctx co
 	return tp.Shutdown, nil
 }
 
-func parseOtelEnvironmentHeaders(fromEnv string) (map[string]string, error) {
-
-	headers := map[string]string{}
-
-	if fromEnv == "" {
-		return headers, nil
-	}
-
-	for _, pair := range strings.Split(fromEnv, ",") {
-		index := strings.Index(pair, "=")
-		if index == -1 {
-			return nil, fmt.Errorf("unable to parse '%s' as a key=value pair, missing a '='", pair)
-		}
-
-		key := strings.TrimSpace(pair[0:index])
-		val := strings.TrimSpace(pair[index+1:])
-
-		headers[key] = val
-	}
-
-	return headers, nil
-}
-
-func createExporter(ctx context.Context) (sdktrace.SpanExporter, error) {
+func createExporter(ctx context.Context) (tracesdk.SpanExporter, error) {
 
 	exporterType := os.Getenv(OtelTraceExporterEnvVar)
 	switch exporterType {
@@ -97,7 +69,7 @@ func createExporter(ctx context.Context) (sdktrace.SpanExporter, error) {
 		return stdouttrace.New(stdouttrace.WithPrettyPrint(), stdouttrace.WithWriter(os.Stderr))
 	}
 
-	endpoint := "localhost:4317"
+	endpoint := ""
 	if val := os.Getenv(OtlpTracesEndpointEnvVar); val != "" {
 		endpoint = val
 	} else if val := os.Getenv(OtlpEndpointEnvVar); val != "" {
@@ -105,94 +77,9 @@ func createExporter(ctx context.Context) (sdktrace.SpanExporter, error) {
 	}
 
 	if strings.HasPrefix(endpoint, "https://") || strings.HasPrefix(endpoint, "http://") {
-
-		u, err := url.Parse(endpoint)
-		if err != nil {
-			return nil, err
-		}
-
-		opts := []otlphttp.Option{}
-
-		hostAndPort := u.Host
-		if u.Port() == "" {
-			if u.Scheme == "https" {
-				hostAndPort += ":443"
-			} else {
-				hostAndPort += ":80"
-			}
-		}
-		opts = append(opts, otlphttp.WithEndpoint(hostAndPort))
-
-		if u.Path == "" {
-			u.Path = "/v1/traces"
-		}
-		opts = append(opts, otlphttp.WithURLPath(u.Path))
-
-		if u.Scheme == "http" {
-			opts = append(opts, otlphttp.WithInsecure())
-		}
-
-		headers, err := parseOtelEnvironmentHeaders(os.Getenv(OtlpHeadersEnvVar))
-		if err != nil {
-			return nil, err
-		}
-		opts = append(opts, otlphttp.WithHeaders(headers))
-
-		return otlphttp.New(ctx, opts...)
+		return otlphttp.New(ctx)
 	} else {
-		opts := []otlpgrpc.Option{}
-
-		opts = append(opts, otlpgrpc.WithEndpoint(endpoint))
-
-		isLocal, err := isLoopbackAddress(endpoint)
-		if err != nil {
-			return nil, err
-		}
-
-		if isLocal {
-			opts = append(opts, otlpgrpc.WithInsecure())
-		}
-
-		headers, err := parseOtelEnvironmentHeaders(os.Getenv(OtlpHeadersEnvVar))
-		if err != nil {
-			return nil, err
-		}
-		opts = append(opts, otlpgrpc.WithHeaders(headers))
-
-		return otlpgrpc.New(ctx, opts...)
+		return otlpgrpc.New(ctx)
 	}
 
-}
-
-func isLoopbackAddress(endpoint string) (bool, error) {
-	hpRe := regexp.MustCompile(`^[\w.-]+:\d+$`)
-	uriRe := regexp.MustCompile(`^(http|https)`)
-
-	endpoint = strings.TrimSpace(endpoint)
-
-	var hostname string
-	if hpRe.MatchString(endpoint) {
-		parts := strings.SplitN(endpoint, ":", 2)
-		hostname = parts[0]
-	} else if uriRe.MatchString(endpoint) {
-		u, err := url.Parse(endpoint)
-		if err != nil {
-			return false, err
-		}
-		hostname = u.Hostname()
-	}
-
-	ips, err := net.LookupIP(hostname)
-	if err != nil {
-		return false, err
-	}
-
-	allAreLoopback := true
-	for _, ip := range ips {
-		if !ip.IsLoopback() {
-			allAreLoopback = false
-		}
-	}
-
-	return allAreLoopback, nil
 }
